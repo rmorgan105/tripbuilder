c76351ae5bed6d1aa863a9b2c6191276715daea3
richard <r_morgan@sympatico.ca>
2017-05-13 11:05:46 -0400

    add fractal and guzzle as dependencies

---
+++ app/Http/Controllers/AirportController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use GuzzleHttp\Psr7\Request;
+
+class AirportController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function resourceList(Request $request)
+    {
+        //todo
+    }
+
+    public function get(Request $request)
+    {
+        //
+    }
+}
